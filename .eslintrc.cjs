e3c82f0101620b6da02f87372953fff6f3622c9b
midedev <ibrahimayomide330@gmail.com>
2023-08-14 12:47:36 +0100

    build: re-initialize proj with vite and typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,18 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: "airbnb",
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
   rules: {
-    quotes: [2, "double", { avoidEscape: true }],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
   },
-};
+}
--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function App() {
-  return <div className="text-red-500">Hi, there</div>;
-}
-
-export default App;
