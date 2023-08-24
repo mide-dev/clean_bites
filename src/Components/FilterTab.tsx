3bf454c11cec04b47384684a4b2892abd982b208
midedev <ibrahimayomide330@gmail.com>
2023-08-24 17:31:13 +0100

    feat: add listener to handle scroll buttons display

--- src/Components/FilterTab.tsx
+++ src/Components/FilterTab.tsx
@@ -1,27 +1,60 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { filterList } from "../constants/filterItems";
-// import { ChevronLeftCircle, ChevronRightCircle } from "lucide-react";
 import Slider from "./Slider";
 
 function FilterTab() {
+  const [showLeftButton, setShowLeftButton] = useState(false);
+  const [showRightButton, setShowRightButton] = useState(false);
   const itemsRef = useRef<HTMLUListElement | null>(null);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (itemsRef.current) {
+        const currentScrollLeft = itemsRef.current.scrollLeft;
+        const currentScrollWidth =
+          itemsRef.current.scrollWidth - itemsRef.current.clientWidth;
+
+        // Check if content exceeds the container's width
+        setShowLeftButton(currentScrollLeft > 0);
+        setShowRightButton(currentScrollLeft < currentScrollWidth);
+      }
+    };
+
+    // Attach the scroll event listener
+    if (itemsRef.current) {
+      itemsRef.current.addEventListener("scroll", handleScroll);
+    }
+
+    // Initial check
+    handleScroll();
+
+    // Cleanup when the component unmounts
+    return () => {
+      if (itemsRef.current) {
+        itemsRef.current.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+  // define left scroll func
   const scrollLeft = () => {
     if (itemsRef.current) {
       itemsRef.current.scrollLeft = itemsRef.current.scrollLeft - 500;
     }
   };
 
+  // define right scoll function
   const scrollRight = () => {
     if (itemsRef.current) {
       itemsRef.current.scrollLeft = itemsRef.current.scrollLeft + 500;
     }
   };
 
+  // handle clicks on filtered items
   const handleItemClick = (item: object) => {
     console.log(item);
   };
 
+  // map and render filters on screen
   const list = filterList.map((item) => (
     <li
       key={item.id}
@@ -36,15 +69,25 @@ function FilterTab() {
   ));
 
   return (
-    <div className="flex container border-2 border-red-400 items-center">
-      <Slider navigation="left" slideLeft={scrollLeft} />
+    <div className="relative flex container border-2 border-red-400 items-center">
+      <Slider
+        navigation="left"
+        slideLeft={scrollLeft}
+        className="hidden sm:inline-block"
+        showLeft={showLeftButton}
+      />
       <ul
-        className="flex gap-x-6 w-full h-[70px] items-center overflow-x-auto scroll-smooth"
+        className="no-scrollbar flex gap-x-6 w-full h-[70px] items-center overflow-x-scroll scroll-smooth"
         ref={itemsRef}
       >
         {list}
       </ul>
-      <Slider navigation="right" slideRight={scrollRight} />
+      <Slider
+        navigation="right"
+        slideRight={scrollRight}
+        className="hidden sm:inline-block"
+        showRight={showRightButton}
+      />
     </div>
   );
 }
--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -5,12 +5,17 @@ type SliderProps = HTMLAttributes<HTMLDivElement> & {
   navigation: "left" | "right";
   slideLeft?: () => void;
   slideRight?: () => void;
+  showLeft?: boolean;
+  showRight?: boolean;
 };
 
 const Slider = ({
   navigation,
   slideLeft,
   slideRight,
+  showLeft,
+  showRight,
+  className,
   ...rest
 }: SliderProps) => {
   // throw error if correct nagivation function is not provided
@@ -23,20 +28,22 @@ const Slider = ({
     );
   }
   return (
-    <div {...rest}>
-      {navigation == "left" && slideLeft ? (
+    <div className={` cursor-pointer ${className}`} {...rest}>
+      {navigation == "left" && showLeft ? (
         <ChevronLeftCircle
           color="#2d2e2f"
           strokeWidth={1}
           onClick={slideLeft}
+          className="absolute top-1/2 left-4 -translate-y-2/4"
         />
       ) : null}
 
-      {navigation == "right" && slideRight ? (
+      {navigation == "right" && showRight ? (
         <ChevronRightCircle
           color="#2d2e2f"
           strokeWidth={1}
           onClick={slideRight}
+          className="absolute top-1/2 right-4 -translate-y-2/4"
         />
       ) : null}
     </div>
