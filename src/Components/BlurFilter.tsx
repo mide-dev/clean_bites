58e14696a4b1f6dd8ecda32b49efc380bf6a8a17
midedev <ibrahimayomide330@gmail.com>
2023-08-26 21:50:51 +0100

    fix: hide blur display on mobile

--- src/Components/BlurFilter.tsx
+++ src/Components/BlurFilter.tsx
@@ -7,11 +7,11 @@ function BlurFilter({ showLeftBlur, showRightBlur }: FilterProps) {
   return (
     <>
       {showLeftBlur && (
-        <div className="absolute w-16 blur-md h-full bg-[#ffffff] left-0"></div>
+        <div className="hidden sm:block absolute w-16 blur-md h-full bg-white left-0"></div>
       )}
 
       {showRightBlur && (
-        <div className="absolute w-16 blur-md h-full bg-[#ffffff] right-0"></div>
+        <div className="hidden sm:block absolute w-16 blur-md h-full bg-white right-0"></div>
       )}
     </>
   );
