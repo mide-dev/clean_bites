8f798edafd5fe4bd0cc1226f3f1783eaead429ca
Mide <ibrahimayomide330@gmail.com>
2023-08-28 19:41:03 +0100

    refactor: chnaged variable names

--- src/Components/ScrollBtnDisplay.tsx
+++ src/Components/ScrollBtnDisplay.tsx
@@ -4,48 +4,48 @@ import { ChevronLeft, ChevronRight } from "lucide-react";
 type conditionProps =
   | {
       navigation: "left";
-      slideLeft?: () => void;
-      showLeft?: boolean;
-      slideRight?: never;
-      showRight?: never;
+      handleLeftClick?: () => void;
+      renderLeftBtn?: boolean;
+      handleRightClick?: never;
+      renderRightBtn?: never;
     }
   | {
       navigation: "right";
-      slideRight?: () => void;
-      showRight?: boolean;
-      slideLeft?: never;
-      showLeft?: never;
+      handleRightClick?: () => void;
+      renderRightBtn?: boolean;
+      handleLeftClick?: never;
+      renderLeftBtn?: never;
     };
 
 type SliderProps = HTMLAttributes<HTMLDivElement> & conditionProps;
 
 const ScrollBtnDisplay = ({
   navigation,
-  slideLeft,
-  slideRight,
-  showLeft,
-  showRight,
+  handleLeftClick,
+  handleRightClick,
+  renderLeftBtn,
+  renderRightBtn,
   className,
   ...rest
 }: SliderProps) => {
   return (
     <div className={`cursor-pointer ${className}`} {...rest}>
-      {navigation == "left" && showLeft ? (
+      {navigation == "left" && renderLeftBtn ? (
         <ChevronLeft
           color="#2d2e2f"
           strokeWidth={2}
           size={26}
-          onClick={slideLeft}
+          onClick={handleLeftClick}
           className="absolute top-1/2 left-8 -translate-y-2/4 bg-white border-[1px] border-custom_primary_100 rounded-full hover:drop-shadow-lg hover:scale-105 hover:duration-150 "
         />
       ) : null}
 
-      {navigation == "right" && showRight ? (
+      {navigation == "right" && renderRightBtn ? (
         <ChevronRight
           color="#2d2e2f"
           strokeWidth={2}
           size={26}
-          onClick={slideRight}
+          onClick={handleRightClick}
           className="absolute top-1/2 right-8 -translate-y-2/4 bg-white border-[1px] border-custom_primary_100 rounded-full hover:drop-shadow-lg hover:scale-105 hover:duration-150 "
         />
       ) : null}
