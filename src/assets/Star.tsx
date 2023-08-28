a709cee9e619b8fcdb15c8815ef8dd190a6e02e3
Mide <ibrahimayomide330@gmail.com>
2023-08-28 19:45:45 +0100

    chore: update assets

---
+++ src/assets/Star.tsx
@@ -0,0 +1,22 @@
+import { SVGProps } from "react";
+
+function Star({ className, ...props }: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="19"
+      height="19"
+      viewBox="0 0 19 19"
+      fill="none"
+      className={`block ${className}`}
+      {...props}
+    >
+      <path
+        d="M9.405 14.4202L3.762 18.81L5.643 11.9138L0 7.524L6.89622 6.89622L9.405 0L11.9138 6.89622L18.81 7.524L13.167 11.9138L15.048 18.81L9.405 14.4202Z"
+        fill="#FF385C"
+      />
+    </svg>
+  );
+}
+
+export default Star;
---
+++ src/assets/hygieneCheck.tsx
@@ -0,0 +1,26 @@
+import { SVGProps } from "react";
+
+function HygieneCheck({ className, ...props }: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="19"
+      height="19"
+      viewBox="0 0 24 24"
+      // fill="white"
+      className={`block ${className}`}
+      {...props}
+    >
+      <path
+        d="M11.97 23.94C18.5808 23.94 23.94 18.5808 23.94 11.97C23.94 5.35915 18.5808 0 11.97 0C5.35915 0 0 5.35915 0 11.97C0 18.5808 5.35915 23.94 11.97 23.94Z"
+        // fill="#288549"
+      />
+      <path
+        d="M17.5559 4.78801L9.17694 13.4064L6.38394 10.5336L3.59094 13.4064L9.17694 19.152L20.3489 7.66081L17.5559 4.78801Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
+
+export default HygieneCheck;
