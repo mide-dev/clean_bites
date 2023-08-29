c8090ac0465bb840437c519d888fad00b08aeaa9
Mide <ibrahimayomide330@gmail.com>
2023-08-29 23:25:47 +0100

    refactor: removed previous style

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -1,18 +1,5 @@
-// export type Place<T = null> = T extends null ? null : {
-//     BusinessName: string;
-//     BusinessType: string;
-//     BusinessTypeID: number;
-//     PostCode: string;
-//     RatingValue: number | string; // Prevent error when the API returns 'no rating'
-//     city: string;
-//     photos: { image: string }[];
-//     place_id: string;
-//     rating: number | string;
-//     street: string;
-//     user_ratings_total: number | string;
-//   };
 
-export type test = {
+export type Data = {
     BusinessName: string;
     BusinessType: string;
     BusinessTypeID: number;
@@ -27,4 +14,4 @@ export type test = {
   };
   
   // Define a type that includes the possibility of null
-  export type Place = test | null;
+  export type Place = Data | null;
