7430f6a175b316ff9d7571ca08258730e28a1b4b
Mide <ibrahimayomide330@gmail.com>
2023-09-05 03:28:31 +0100

    feat: update api to catch error

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,6 +2,13 @@ import { Place } from "./types";
 
 export async function getPlaces(): Promise<Place[]> {
     const response = await fetch("/api/places");
+    if (!response.ok) {
+      throw {
+        message: "Failed to fetch Places",
+        statusText: response.statusText,
+        status: response.status
+      }
+    }
     // simulate slow fetch
     await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await response.json();
