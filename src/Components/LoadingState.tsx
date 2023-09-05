085ca87834e26b6886036f3ca71a11d6f1164ff6
Mide <ibrahimayomide330@gmail.com>
2023-09-05 03:24:13 +0100

    feat: build skeleton layout for loading state

---
+++ src/Components/LoadingState.tsx
@@ -0,0 +1,49 @@
+import { Skeleton } from "./ui/skeleton";
+
+function createArray(rowCount: number) {
+  const arr = [];
+
+  for (let i = 0; i < rowCount; i++) {
+    arr.push(i);
+  }
+
+  return arr;
+}
+
+function LoadingState() {
+  const columnCount = 40;
+  const myArray = createArray(columnCount);
+
+  return (
+    <>
+      <div className="places-grid">
+        {myArray.map((item) => {
+          return (
+            <div key={item}>
+              <Skeleton
+                className="bg-custom_primary_50 rounded-t-lg rounded-b-none min-w-[100%] 
+                h-[200px] xs:h-[260px] md:h-[220px] lg:h-[200px]"
+              />
+              <div className="flex justify-between pt-2">
+                <Skeleton className="bg-custom_primary_50 rounded-md w-20 h-4" />
+                <Skeleton className="bg-custom_primary_50 rounded-md w-28 h-4" />
+              </div>
+              <div className="pt-3">
+                <Skeleton className="bg-custom_primary_50 rounded-md w-28 h-4 " />
+              </div>
+              <div className="pt-3">
+                <Skeleton className="bg-custom_primary_50 rounded-md w-16 h-3 " />
+              </div>
+              <div className="pt-4 flex gap-x-2 items-center">
+                <Skeleton className="bg-custom_primary_50 rounded-full w-6 h-6 " />
+                <Skeleton className="bg-custom_primary_50 rounded-md w-32 h-4 " />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default LoadingState;
