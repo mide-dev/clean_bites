7fa07ee7cb17aa262893d487f876050ffd4e9108
Mide <ibrahimayomide330@gmail.com>
2023-09-06 12:59:25 +0100

    feat: add indicator to display all images in place card

--- src/Components/PlaceImage.tsx
+++ src/Components/PlaceImage.tsx
@@ -1,5 +1,6 @@
 import { useState, HTMLAttributes } from "react";
 import ScrollBtnDisplay from "./ScrollBtnDisplay";
+import Favorite from "./Favorite";
 
 type PlaceImageProp = HTMLAttributes<HTMLImageElement> & {
   images: { image: string }[];
@@ -20,21 +21,50 @@ function PlaceImage({ images, ...props }: PlaceImageProp) {
     }
   };
 
+  // set current image
   const photo = images[currentImageIndex].image;
+
+  // display how many images are present
+  const totalImageIndicator = (imageArray: object[]) => {
+    const totalImageArray = [];
+    // save image length in totalImageArray
+    for (let i = 0; i < imageArray.length; i++) {
+      totalImageArray.push(i);
+    }
+    // if image len greater than 1, display total image indicator
+    return (
+      totalImageArray.length > 1 &&
+      totalImageArray.map((imageNo) => (
+        <div
+          key={imageNo}
+          className={`rounded-full  h-1.5 w-1.5 ${
+            imageNo === currentImageIndex ? "bg-white" : "bg-gray-300"
+          }`}
+        ></div>
+      ))
+    );
+  };
+
   return (
     <div className="relative">
       <ScrollBtnDisplay
         navigation="left"
         handleLeftClick={handlePrevClick}
         renderLeftBtn={currentImageIndex > 0}
-        className="left-4"
+        className="left-4 opacity-90 imageBtnScroll"
       />
       <img {...props} src={photo} />
+      <div className="flex items-center gap-x-2 centered-element">
+        {totalImageIndicator(images)}
+      </div>
+      <div className="absolute top-2 right-4">
+        <Favorite />
+      </div>
       <ScrollBtnDisplay
         navigation="right"
         handleRightClick={handleNextClick}
         renderRightBtn={currentImageIndex < images.length - 1}
-        className="right-4"
+        className="right-4 opacity-90 imageBtnScroll"
       />
     </div>
   );
