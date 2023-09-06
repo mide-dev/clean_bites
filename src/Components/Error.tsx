fe9eeb6fd716bfbcc260b7e9a7603d9bc7a24309
Mide <ibrahimayomide330@gmail.com>
2023-09-06 13:08:59 +0100

    Refactor: remove header display

--- src/Components/Error.tsx
+++ src/Components/Error.tsx
@@ -1,4 +1,3 @@
-// import Header from "./Header";
 import { useRouteError } from "react-router-dom";
 
 type Error = {
@@ -11,7 +10,6 @@ function Error() {
   const error = useRouteError() as Error;
   return (
     <>
-      {/* <Header /> */}
       <div className="grid place-content-center border-2 w-full h-screen">
         <h1 className="text-custom_primary_500 font-medium">
           An error occured: {error.message}
