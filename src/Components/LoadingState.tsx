6fdcbd658a79b2da3ba039b6485a3750763a58a0
Mide <ibrahimayomide330@gmail.com>
2023-09-06 13:03:32 +0100

    feat: update loading skeleton to resemble appearance of place card

--- src/Components/LoadingState.tsx
+++ src/Components/LoadingState.tsx
@@ -21,22 +21,22 @@ function LoadingState() {
           return (
             <div key={item}>
               <Skeleton
-                className="bg-custom_primary_50 rounded-t-lg rounded-b-none min-w-[100%] 
+                className="bg-gray-300 rounded-t-lg rounded-b-none min-w-[100%] 
                 h-[200px] xs:h-[260px] md:h-[220px] lg:h-[200px]"
               />
               <div className="flex justify-between pt-2">
-                <Skeleton className="bg-custom_primary_50 rounded-md w-20 h-4" />
-                <Skeleton className="bg-custom_primary_50 rounded-md w-28 h-4" />
+                <Skeleton className="bg-gray-300 rounded-md w-20 h-4" />
+                <Skeleton className="bg-gray-300 rounded-md w-28 h-4" />
               </div>
               <div className="pt-3">
-                <Skeleton className="bg-custom_primary_50 rounded-md w-28 h-4 " />
+                <Skeleton className="bg-gray-300 rounded-md w-28 h-4 " />
               </div>
               <div className="pt-3">
-                <Skeleton className="bg-custom_primary_50 rounded-md w-16 h-3 " />
+                <Skeleton className="bg-gray-300 rounded-md w-16 h-3 " />
               </div>
               <div className="pt-4 flex gap-x-2 items-center">
-                <Skeleton className="bg-custom_primary_50 rounded-full w-6 h-6 " />
-                <Skeleton className="bg-custom_primary_50 rounded-md w-32 h-4 " />
+                <Skeleton className="bg-gray-300 rounded-full w-6 h-6 " />
+                <Skeleton className="bg-gray-300 rounded-md w-32 h-4 " />
               </div>
             </div>
           );
