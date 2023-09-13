787562f8085232b482b6ee1854589a014fbeb656
Mide <ibrahimayomide330@gmail.com>
2023-09-13 12:08:04 +0100

    fix: blur position on multiple breakpointsg

--- src/Components/BlurFilter.tsx
+++ src/Components/BlurFilter.tsx
@@ -1,17 +1,19 @@
-type FilterProps = {
+import {HTMLAttributes } from "react";
+
+type FilterProps = HTMLAttributes<HTMLDivElement> & {
   showLeftBlur?: boolean;
   showRightBlur?: boolean;
 };
 
-function BlurFilter({ showLeftBlur, showRightBlur }: FilterProps) {
+function BlurFilter({ showLeftBlur, showRightBlur, className }: FilterProps) {
   return (
     <>
       {showLeftBlur && (
-        <div className="hidden sm:block absolute w-16 blur-md h-full bg-white left-0"></div>
+        <div className={`hidden sm:block absolute w-16 blur-md h-full bg-white left-0 ${className}`}></div>
       )}
 
       {showRightBlur && (
-        <div className="hidden sm:block absolute w-16 blur-md h-full bg-white right-0"></div>
+        <div className={`hidden sm:block absolute w-16 blur-md h-full bg-white right-0 ${className}`}></div>
       )}
     </>
   );
