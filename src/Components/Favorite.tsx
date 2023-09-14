2983ede06f7c8f486778973439320791cd89e54f
Mide <ibrahimayomide330@gmail.com>
2023-09-14 20:02:37 +0100

    feat: allow component accept css props

--- src/Components/Favorite.tsx
+++ src/Components/Favorite.tsx
@@ -1,7 +1,8 @@
+import { HTMLAttributes } from "react";
 import Heart from "../assets/Heart";
 
-function Favorite() {
-  return <Heart className="fill-gray-500 stroke-white opacity-85" />;
+function Favorite({className}: HTMLAttributes<HTMLDivElement>) {
+  return <Heart className={`fill-gray-500 stroke-white opacity-85 ${className}`} />;
 }
 
 export default Favorite;
