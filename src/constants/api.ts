307d1281422c4e6de029cf782051db5e17fdfc58
Mide <ibrahimayomide330@gmail.com>
2023-09-14 20:11:12 +0100

    feat: no changes

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,11 @@
 import { Place } from "./types";
 
+// type ApiProp = {
+//   id?: number
+// }
+
 export async function getPlaces(): Promise<Place[]> {
+ 
     const response = await fetch("/api/places");
     if (!response.ok) {
       throw {
@@ -10,7 +15,23 @@ export async function getPlaces(): Promise<Place[]> {
       }
     }
     // simulate slow fetch
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
     const data = await response.json();
-    return data;
+    return data.places;
+  
+
+  // 
+  // const response = await fetch("/api/placesDetail"); 
+  //   if (!response.ok) {
+  //     throw {
+  //       message: "Failed to fetch Places",
+  //       statusText: response.statusText,
+  //       status: response.status
+  //     }
+  //   }
+  //   // simulate slow fetch
+  //   // await new Promise((resolve) => setTimeout(resolve, 3000));
+  //   const data = await response.json();
+  //   return data;
   }
+  
