e6fef1e03c8c2a17ff0857e54511494729393a14
Mide Ibrahim <ibrahimayomide330@gmail.com>
2023-09-20 14:07:32 +0000

    feat: render map

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -1,20 +1,29 @@
-import {useState, useMemo, useCallback, useRef} from "react";
-import {GoogleMap, Marker, MarkerClusterer } from "react-google-maps";
-import { useLoadScript } from "@react-google-maps/api";
-
-type LatLngLiteral = google.maps.LatLngLiteral;
-type MapOptions = google.maps.MapOptions;
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+import { useMemo } from "react";
 
 function Map() {
-
-    const {isLoaded} = useLoadScript({
-        googleMapsApiKey: import.meta.env.VITE_FOOD_APP_GOOGLE_MAP_API_KEY,
-    });
-
-    // loading state
-    if (!isLoaded) return <div>Loading...</div>
-
-    return ( <div>map</div> );
+    const { isLoaded } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_FOOD_APP_GOOGLE_MAP_API_KEY,
+  });
+  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
+  console.log(import.meta.env.VITE_FOOD_APP_GOOGLE_MAP_API_KEY)
+  const containerStyle = {
+    width: "100%",
+    height: "400px",
+}
+  return (
+    <div className="w-[700px] h-[700px] mb-20">
+      {!isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <GoogleMap
+        mapContainerStyle={containerStyle}
+          center={center}
+          zoom={10}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Map;
