c3e498a66933276225b7a7910df3e890bf2ecb39
Mide <ibrahimayomide330@gmail.com>
2023-09-21 17:27:36 +0100

    fix: disable dummy server call to fix map display issue

--- src/Pages/PlaceDetail.tsx
+++ src/Pages/PlaceDetail.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Header from "@/Components/Header";
-import Map from '@/Components/Map/Map'
+import Map from "@/Components/Map/Map";
 import PlaceImage from "@/Components/PlaceImage";
 import PlaceOffers from "@/Components/PlaceOffers";
 import { UtensilsCrossed, PhoneCall } from "lucide-react";
@@ -9,9 +9,14 @@ import { useHygieneCheck, HygieneProp } from "../constants/HygieneCheck";
 import HygieneIcon from "@/assets/hygieneIcon";
 import Star from "@/assets/Star";
 
+import placesDetail from "../../mockData/PlacesDetail.json";
 
 function PlaceDetail() {
   const [placeDetail, setPlaceDetail] = useState(null);
+
+  useEffect(() => {
+    setPlaceDetail(placesDetail[0]);
+  }, []);
   // const [hygieneData, setHygieneData] = useState<HygieneProp | null>(null);
 
   // // process & retrieve hygiene data
@@ -21,15 +26,14 @@ function PlaceDetail() {
   //   setHygieneData(hygieneResult);
   // }, []);
 
-  useEffect(() => {
-    fetch("api/placeDetail/1")
-      .then((res) => res.json())
-      .then((data) => {
-        setPlaceDetail(data.placeDetails);
-      });
-  }, []);
-  console.log(placeDetail)
-
+  // useEffect(() => {
+  //   fetch("api/placeDetail/1")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setPlaceDetail(data.placeDetails);
+  //     });
+  // }, []);
+  // if (placeDetail) console.log(placeDetail[0].offers);
 
   if (placeDetail) {
     return (
@@ -37,54 +41,60 @@ function PlaceDetail() {
         <Header className="hidden md:flex is-place-detail" />
         <main className="md:container is-place-detail text-sm">
           <div className="placeCard">
-            {<PlaceImage images={placeDetail.photos} className="max-h-[580px] w-full object-cover" />}
+            {
+              <PlaceImage
+                images={placeDetail.photos}
+                className="max-h-[580px] w-full object-cover"
+              />
+            }
           </div>
-            {/* PLACE INFO */}
-            <section>
-              {/*  title */}
-              <div className="flex items-center">
-                <h1>
-                  {placeDetail.BusinessName}<span className="px-2">&#8226;</span>
-                </h1>
-                <div className="text-base">$$$</div>
-              </div>
-              {/* google review group */}
-              <div className="flex gap-x-[0.1rem]">
-                <Star className="w-4 fill-black inline-block" />
-                <p className="inline-block">
-                  4.92 <span className="px-[0.2rem]">&#8226;</span>
-                  <button className="underline">723 Google Reviews </button>
-                  <span className="px-[0.2rem]">&#8226;</span> 60 Spring Gardens
-                </p>
-              </div>
-              {/* hygiene */}
-              <div>
-                <HygieneIcon className="" />
-                <p>Health & Hygiene Rating - Very Poor</p>
-                <div className="progress-bar w-full h-5 bg-[#ccc] rounded-full">
-                  <div className="grid place-content-center progress-fill h-full bg-red-500 w-[60%] rounded-full">
-                    <p className="text-white font-medium">60% </p>
-                  </div>
+          {/* PLACE INFO */}
+          <section>
+            {/*  title */}
+            <div className="flex items-center">
+              <h1>
+                {placeDetail.BusinessName}
+                <span className="px-2">&#8226;</span>
+              </h1>
+              <div className="text-base">$$$</div>
+            </div>
+            {/* google review group */}
+            <div className="flex gap-x-[0.1rem]">
+              <Star className="w-4 fill-black inline-block" />
+              <p className="inline-block">
+                4.92 <span className="px-[0.2rem]">&#8226;</span>
+                <button className="underline">723 Google Reviews </button>
+                <span className="px-[0.2rem]">&#8226;</span> 60 Spring Gardens
+              </p>
+            </div>
+            {/* hygiene */}
+            <div>
+              <HygieneIcon className="" />
+              <p>Health & Hygiene Rating - Very Poor</p>
+              <div className="progress-bar w-full h-5 bg-[#ccc] rounded-full">
+                <div className="grid place-content-center progress-fill h-full bg-red-500 w-[60%] rounded-full">
+                  <p className="text-white font-medium">60% </p>
                 </div>
               </div>
-              {/* open time */}
-              <div className="flex gap-x-2 items-center">
-                <UtensilsCrossed className="w-5 stroke-custom_primary_500" />
-                <p>Open 11:00 AM - 11:00 PM</p>
-                <span>&#8226;</span>
-                <button className="underline">See hours</button>
-              </div>
-              {/* contact call */}
-              <div className="flex gap-x-2 items-center ">
-                <PhoneCall className="w-5 stroke-custom_primary_500" />
-                <p>0191 260 5880</p>
-              </div>
-            </section>
-    
-            {/* PLACE OFFERS */}
-            {<PlaceOffers data ={placeDetail.offers[0]} />}
-            <div className="h-20 w-4 " ></div>
-            <Map />
+            </div>
+            {/* open time */}
+            <div className="flex gap-x-2 items-center">
+              <UtensilsCrossed className="w-5 stroke-custom_primary_500" />
+              <p>Open 11:00 AM - 11:00 PM</p>
+              <span>&#8226;</span>
+              <button className="underline">See hours</button>
+            </div>
+            {/* contact call */}
+            <div className="flex gap-x-2 items-center ">
+              <PhoneCall className="w-5 stroke-custom_primary_500" />
+              <p>0191 260 5880</p>
+            </div>
+          </section>
+
+          {/* PLACE OFFERS */}
+          {<PlaceOffers data={placeDetail.offers[0]} />}
+          {/* <div className="h-20 w-4 "></div> */}
+          <Map />
         </main>
       </>
     );
