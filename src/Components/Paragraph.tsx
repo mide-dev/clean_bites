f2172bc85ecf1d35d2ae2c99d82d13bffffee30f
Mide Ibrahim <ibrahimayomide330@gmail.com>
2023-09-21 23:07:41 +0000

    feat: create component for long paragraphs

---
+++ src/Components/Paragraph.tsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react"
+
+function Paragraph({text}) {
+    const [showMore, setShowMore] = useState(false);
+    const [Str, setStr] = useState('');
+
+    
+        useEffect(()=>{
+            if (text.length > 38) {
+                setStr(text.substring(0, 38))
+            }
+        }, [])
+        const handleClick = ()=> {
+            setShowMore(!showMore)
+        }
+
+    // console.log(text, Str)
+
+    if (Str) {
+        return (
+            <>
+                <p>
+                    {showMore ? text : `${Str}...`}  
+                </p>
+                <button  
+                    // className={truncatedStr ? 'inline-block': 'hidden'}
+                    onClick={handleClick}>{showMore ? 'Show Less': 'Show More'}
+                </button>
+            </>
+        )
+    }
+}
+
+export default Paragraph
