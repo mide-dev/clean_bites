23e15aaea0bac1e214b2b047352d6a9a58683bc3
Mide Ibrahim <ibrahimayomide330@gmail.com>
2023-09-21 23:03:16 +0000

    feat: update maps display setting

--- src/Components/Map/Map.tsx
+++ src/Components/Map/Map.tsx
@@ -1,25 +1,40 @@
+import { useMemo, HTMLAttributes } from "react";
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
-import { useMemo } from "react";
 
-function Map() {
+type MapProps = HTMLAttributes<HTMLHeadElement> & {
+  latitude:number; 
+  longitude:number;
+ }
+
+ type MapOptions = google.maps.MapOptions
+
+function Map({className, latitude, longitude}: MapProps) {
   const { isLoaded } = useLoadScript({
-    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAP_API_KEY,
+    googleMapsApiKey: import.meta.env.VITE_FOOD_APP_GOOGLE_MAP_API_KEY,
   });
-  const center = useMemo(() => ({ lat: 18.52043, lng: 73.856743 }), []);
-  // console.log(import.meta.env.VITE_GOOGLE_MAP_API_KEY);
+  const center = useMemo(() => ({ lat: latitude, lng: longitude }), [latitude, longitude]);
+  const options = useMemo<MapOptions>(() => ({
+    mapId: "acf0cee82df6cc02",
+    minZoom: 14,
+    maxZoom: 17,
+    disableDefaultUI: true,
+    clickableIcons: false
+  }), [])
+
   const containerStyle = {
     width: "100%",
-    height: "400px",
+    height: "100%",
   };
   return (
-    <div className="w-[700px] h-[700px] mb-20">
+    <div className={`flex ${className}`}>
       {!isLoaded ? (
         <h1>Loading...</h1>
       ) : (
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={center}
-          zoom={16}
+          zoom={15}
+          options ={options}
         />
       )}
     </div>
