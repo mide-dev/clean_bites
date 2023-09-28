249da765434cf3d7041e27bc79c4d157314c2c7b
Mide <ibrahimayomide330@gmail.com>
2023-09-28 21:04:55 +0100

    feat: rebuilt component to allow mobile swipe and smooth transition

--- src/Components/PlaceImage.tsx
+++ src/Components/PlaceImage.tsx
@@ -1,4 +1,4 @@
-import { useState, HTMLAttributes } from "react";
+import { useState, HTMLAttributes, useEffect, useRef } from "react";
 import ScrollBtnDisplay from "./ScrollBtnDisplay";
 import Favorite from "./Favorite";
 
@@ -6,39 +6,7 @@ type PlaceImageProp = HTMLAttributes<HTMLImageElement> & {
   images: { image: string }[];
 };
 
-import ImageGallery from "react-image-gallery";
-// import stylesheet if you're not already using CSS @import
-import "react-image-gallery/styles/css/image-gallery.css";
-import React from "react";
-
-const images = [
-  {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    // thumbnail: "https://picsum.photos/id/1018/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    // thumbnail: "https://picsum.photos/id/1015/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    // thumbnail: "https://picsum.photos/id/1019/250/150/",
-  },
-];
-
-class PlaceImage extends React.Component {
-  render() {
-    return <ImageGallery items={images} 
-              showFullscreenButton={false} 
-              showPlayButton={false} 
-              showBullets={true} 
-              showIndex={true}
-              disableKeyDown={true}/>;
-  }
-}
-export default PlaceImage
-
-// function PlaceImage({ images, ...props }: PlaceImageProp) {
+function PlaceImage({ images, ...props }: PlaceImageProp) {
   // const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   // const handlePrevClick = () => {
@@ -53,10 +21,57 @@ export default PlaceImage
   //   }
   // };
 
-  // // set current image
+  // set current image
   // const photo = images[currentImageIndex].image;
+  const [showLeftButton, setShowLeftButton] = useState(false);
+  const [showRightButton, setShowRightButton] = useState(false);
+  const itemsRef = useRef<HTMLDivElement | null>(null);
 
-  // // display how many images are present
+  // listen for scroll event to determine scroll btn display
+  useEffect(() => {
+    const handleScroll = () => {
+      if (itemsRef.current) {
+        const currentScrollLeft = itemsRef.current.scrollLeft;
+        const currentScrollWidth =
+          itemsRef.current.scrollWidth - itemsRef.current.clientWidth;
+
+        // Check if content exceeds the container's width and set true/false
+        setShowLeftButton(currentScrollLeft > 0);
+        setShowRightButton(currentScrollLeft < currentScrollWidth);
+      }
+    };
+
+    // Attach the scroll event listener
+    if (itemsRef.current) {
+      itemsRef.current.addEventListener("scroll", handleScroll);
+    }
+
+    // Initial check
+    handleScroll();
+
+    // Cleanup when the component unmounts
+    return () => {
+      if (itemsRef.current) {
+        itemsRef.current.removeEventListener("scroll", handleScroll);
+      }
+    };
+  }, []);
+
+  // define left scroll func
+  const scrollLeft = () => {
+    if (itemsRef.current) {
+      itemsRef.current.scrollLeft = itemsRef.current.scrollLeft - 500;
+    }
+  };
+
+  // define right scoll function
+  const scrollRight = () => {
+    if (itemsRef.current) {
+      itemsRef.current.scrollLeft = itemsRef.current.scrollLeft + 500;
+    }
+  };
+
+  // display how many images are present
   // const totalImageIndicator = (imageArray: object[]) => {
   //   const totalImageArray = [];
   //   // save image length in totalImageArray
@@ -77,27 +92,63 @@ export default PlaceImage
   //   );
   // };
 
-  // return (
-  //   <div className="relative">
-  //     <ScrollBtnDisplay
-  //       navigation="left"
-  //       handleLeftClick={handlePrevClick}
-  //       renderLeftBtn={currentImageIndex > 0}
-  //       className="imageBtnScroll left-4 opacity-90"
-  //     />
-  //     <img {...props} src={photo} />
-  //     <div className="flex items-center gap-x-2 centered-element">
-  //       {totalImageIndicator(images)}
-  //     </div>
-      
-  //     <ScrollBtnDisplay
-  //       navigation="right"
-  //       handleRightClick={handleNextClick}
-  //       renderRightBtn={currentImageIndex < images.length - 1}
-  //       className="imageBtnScroll right-4 opacity-90 "
-  //     />
-  //   </div>
-  // );
-// }
+  const renderSlider = images.map((img) => (
+    <button
+      key={images.indexOf(img)}
+      className="w-2 h-2 rounded-full bg-white opacity-75 transition-opacity ease-in-out 
+        duration-200 hover:opacity-100"
+      // onClick={}
+    ></button>
+  ));
 
-// export default PlaceImage;
+  const renderImages = images.map((img) => (
+    <img key={images.indexOf(img)} id="slide-2" src={img.image} />
+  ));
+
+  return (
+    <div className="relative">
+      <div className="slider-wrapper relative mx-auto">
+        <div className="slider no-scrollbar md:rounded-xl" ref={itemsRef}>
+          {renderImages}
+        </div>
+        <ScrollBtnDisplay
+          navigation="left"
+          handleLeftClick={scrollLeft}
+          renderLeftBtn={showLeftButton}
+          className="hidden sm:inline-block xs:left-8 md:left-[2.5rem] 2xl:left-20 
+        border-[1px] border-custom_primary_100"
+        />
+        <div className="slider-nav flex gap-x-2 absolute bottom-[1.25rem] left-2/4 -translate-x-2/4 z-10">
+          {renderSlider}
+        </div>
+        <ScrollBtnDisplay
+          navigation="right"
+          handleRightClick={scrollRight}
+          renderRightBtn={showRightButton}
+          className="hidden sm:inline-block xs:right-8 md:right-[2.5rem] 2xl:right-20 
+        border-[1px] border-custom_primary_100"
+        />
+      </div>
+
+      {/* <ScrollBtnDisplay
+        navigation="left"
+        handleLeftClick={handlePrevClick}
+        renderLeftBtn={currentImageIndex > 0}
+        className="imageBtnScroll left-4 opacity-90"
+      />
+      <img {...props} src={photo} />
+      <div className="flex items-center gap-x-2 centered-element">
+        {totalImageIndicator(images)}
+      </div>
+
+      <ScrollBtnDisplay
+        navigation="right"
+        handleRightClick={handleNextClick}
+        renderRightBtn={currentImageIndex < images.length - 1}
+        className="imageBtnScroll right-4 opacity-90 "
+      /> */}
+    </div>
+  );
+}
+
+export default PlaceImage;
