f778d30546231d66fcb1223355527f8d30fee995
Mide <ibrahimayomide330@gmail.com>
2023-10-02 17:32:06 +0100

    style: remove conditional breakpoint rendering

--- src/Components/BlurFilter.tsx
+++ src/Components/BlurFilter.tsx
@@ -1,4 +1,4 @@
-import {HTMLAttributes } from "react";
+import { HTMLAttributes } from "react";
 
 type FilterProps = HTMLAttributes<HTMLDivElement> & {
   showLeftBlur?: boolean;
@@ -9,11 +9,15 @@ function BlurFilter({ showLeftBlur, showRightBlur, className }: FilterProps) {
   return (
     <>
       {showLeftBlur && (
-        <div className={`hidden sm:block absolute w-16 blur-md h-full bg-white left-0 ${className}`}></div>
+        <div
+          className={`absolute w-16 blur-md h-full bg-white left-0 ${className}`}
+        ></div>
       )}
 
       {showRightBlur && (
-        <div className={`hidden sm:block absolute w-16 blur-md h-full bg-white right-0 ${className}`}></div>
+        <div
+          className={`absolute w-16 blur-md h-full bg-white right-0 ${className}`}
+        ></div>
       )}
     </>
   );
