232c5a466cfe0938e62912d085e926aa50549dab
Mide <ibrahimayomide330@gmail.com>
2023-10-02 17:58:08 +0100

    no change

--- src/Components/RenderFilters.tsx
+++ src/Components/RenderFilters.tsx
@@ -1,22 +1,22 @@
-import { filterList } from "@/constants/filterItems";
+// import { filterList } from "@/constants/filterItems";
 
 type RenderFiltersProp = {
-  handleItemClick: (item: object) => void;
+  // handleItemClick: (item: object) => void;
 };
 
-function RenderFilters({ handleItemClick }: RenderFiltersProp) {
+function RenderFilters({ list }) {
   return (
     <>
-      {filterList.map((item) => (
+      {list.map((item) => (
         <li
           key={item.id}
           className="flex flex-col justify-center items-center whitespace-nowrap gap-y-2 
             cursor-pointer text-[12px] text-[#717171] font-poppins hover:text-black 
             stroke-custom_primary_400 first:pl-4 sm:first:pl-0 last:pr-4 
             sm:last:pr-0"
-          onClick={() => {
-            handleItemClick(item);
-          }}
+          // onClick={() => {
+          //   handleItemClick(item);
+          // }}
         >
           <item.icon strokeWidth={1.5} size={24} className="" />
           {item.item}
