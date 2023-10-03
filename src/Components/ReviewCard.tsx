454f82af5e3fd3188fbe23e0422b4e115ac415f6
Mide Ibrahim <ibrahimayomide330@gmail.com>
2023-10-03 17:48:23 +0000

    style: remove border on all screen size

--- src/Components/ReviewCard.tsx
+++ src/Components/ReviewCard.tsx
@@ -8,7 +8,7 @@ import Star from "@/assets/Star";
 
 function ReviewCard({ data }) {
   return (
-    <Card className="p-3 md:border-none">
+    <Card className="p-3 border-none">
       <div className="flex gap-x-2 items-center">
         <Avatar className="w-12 h-12">
           <AvatarImage src={data.profile_photo_url} />
