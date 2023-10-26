89799335a735fbc7bf044dce05f4199aa2a4714f
Mide <ibrahimayomide330@gmail.com>
2023-10-26 15:01:01 +0100

    refactor:

--- src/Pages/PlaceDetail.tsx
+++ src/Pages/PlaceDetail.tsx
@@ -21,16 +21,18 @@ import Divider from "@/Components/Divider";
 function PlaceDetail() {
   const [placeDetail, setPlaceDetail] = useState(null);
 
+  // process & retrieve hygiene data
+  const hygieneResult = useHygieneCheck(placeDetail?.RatingValue);
+
   useEffect(() => {
     setPlaceDetail(placesDetail[0]);
   }, []);
- 
-  
+
   if (placeDetail) {
     return (
       <>
         <Header className="hidden md:flex is-place-detail" />
-        <Divider axis='horizontal' className="hidden md:block"/>
+        <Divider axis="horizontal" className="hidden md:block" />
         <main className="md:container is-place-detail text-sm mb-10">
           {/* PLACE INFO */}
           <section className="flex flex-col">
@@ -45,7 +47,7 @@ function PlaceDetail() {
             </div>
             {/*  title */}
             <div className="flex items-center place_detail_section md:order-1">
-              <h1>
+              <h1 className="my-2">
                 {placeDetail.BusinessName}
                 <span className="px-2">&#8226;</span>
               </h1>
@@ -53,26 +55,55 @@ function PlaceDetail() {
             </div>
             {/* hygiene */}
             <div className="place_detail_section md:order-4">
-              <HygieneIcon className="" />
-              <p>Health & Hygiene Rating - Very Poor</p>
-              <div className="progress-bar w-full h-5 bg-[#ccc] rounded-full">
-                <div className="grid place-content-center progress-fill h-full bg-red-500 w-[60%] rounded-full">
-                  <p className="text-white font-medium">60% </p>
+              {/* conditionally render hygiene */}
+              {hygieneResult === "Awaiting Inspection" ? (
+                <div className={`flex pt-2 items-center gap-x-2 py-2`}>
+                  Health & Hygiene Result -
+                  <p className="block font-medium">Awaiting Inspection</p>
                 </div>
-              </div>
+              ) : (
+                <>
+                  <div className="flex gap-x-1 pb-3">
+                    <HygieneIcon className={`${hygieneResult.fill}`} />
+                    <p>
+                      Health & Hygiene Result -{" "}
+                      <span className="font-medium">
+                        {hygieneResult.ratingText}
+                      </span>
+                    </p>
+                  </div>
+                  <div className="progress-bar w-full h-5 bg-[#ccc] rounded-full">
+                    <div
+                      className={`grid place-content-center progress-fill h-full w-${hygieneResult.hygieneScore}% rounded-full ${hygieneResult.bg}`}
+                    >
+                      <p className="text-white font-medium">
+                        {hygieneResult.hygieneScore}%
+                      </p>
+                    </div>
+                  </div>
+                  {/* mobile-divider */}
+                  <Divider
+                    axis="horizontal"
+                    className="bg-slate-300 md:hidden"
+                  />
+                </>
+              )}
             </div>
-            {/* mobile-divider */}
-            <Divider axis='horizontal' className="bg-slate-300 md:hidden"/>
+
             {/* google review group */}
             <div className=" gap-x-[0.1rem] place_detail_section md:order-2">
               <div className="flex">
                 <Star className="w-4 fill-black inline-block" />
                 <p className="inline-block">
                   4.92 <span className="px-[0.2rem] inline-block">&#8226;</span>
-                  <button className="underline inline-block">723 Google Reviews</button>
+                  <button className="underline inline-block">
+                    723 Google Reviews
+                  </button>
                 </p>
               </div>
-              <p>60 Spring Gardens,<span className="px-[0.2rem]"></span> London</p>
+              <p className="pt-1">
+                60 Spring Gardens,<span className="px-[0.2rem]"></span> London
+              </p>
             </div>
             {/* open time */}
             <div className="flex gap-x-2 items-center place_detail_section md:order-5">
@@ -82,11 +113,13 @@ function PlaceDetail() {
               <button className="underline">See hours</button>
             </div>
             {/* contact call */}
-            <div className="flex gap-x-2 items-center place_detail_section md:order-6">
-              <PhoneCall className="w-5 stroke-custom_primary_500" />
-              <p>0191 260 5880</p>
+            <div className="place_detail_section md:order-6">
+              <div className="flex gap-x-2 items-center">
+                <PhoneCall className="w-5 stroke-custom_primary_500" />
+                <p>0191 260 5880</p>
+              </div>
               {/* mobile-divider */}
-              <Divider axis='horizontal' className="bg-slate-300 md:hidden"/>
+              <Divider axis="horizontal" className="bg-slate-300 md:hidden" />
             </div>
           </section>
 
@@ -94,7 +127,7 @@ function PlaceDetail() {
           <section className="place_detail_section">
             <h2>What this place offers</h2>
             <PlaceOffers data={placeDetail.offers[0]} />
-            <Divider axis='horizontal' className="bg-slate-300"/>
+            <Divider axis="horizontal" className="bg-slate-300" />
           </section>
           {/* Place Map */}
           <section className="place_detail_section">
@@ -106,6 +139,7 @@ function PlaceDetail() {
               longitude={0.1276}
               className="mx-auto w-full h-[220px] sm:h-[300px] md:h-[450px]"
             />
+            <Divider axis="horizontal" className="bg-slate-300" />
           </section>
           {/* place review */}
           <section className="place_detail_section">
@@ -123,7 +157,13 @@ function PlaceDetail() {
             </Button>
             <div>
               {placeDetail.reviews.map((review) => (
-                <ReviewCard data={review} key={review.time} />
+                <>
+                  <ReviewCard data={review} key={review.time} />
+                  <Divider
+                    axis="horizontal"
+                    className="bg-slate-300 last:hidden"
+                  />
+                </>
               ))}
             </div>
             <Button
@@ -133,9 +173,13 @@ function PlaceDetail() {
               <span>More reviews on Google</span>
               <ChevronRight className="stroke-slate-700" />
             </Button>
+            <Divider axis="horizontal" className="bg-slate-300" />
             {/* Explore */}
             <div className="bg-[#FFFBFC] md:bg-white rounded-xl ">
-              <h2>Explore places similar to <span className="font-medium">{placeDetail.BusinessName}</span></h2>
+              <h2>
+                Explore places similar to{" "}
+                <span className="font-medium">{placeDetail.BusinessName}</span>
+              </h2>
               <div className="rounded-xl overflow-scroll no-scrollbar scroll-smooth">
                 <PlaceRecommendation
                   place_id={placeDetail.place_id}
