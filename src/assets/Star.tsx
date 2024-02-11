3f4256e263b6fca00cd157f5aea6d06cd2d1b32d
Mide <ibrahimayomide330@gmail.com>
2024-02-11 21:54:07 +0000

    style: remove width and height

--- src/assets/Star.tsx
+++ src/assets/Star.tsx
@@ -4,8 +4,6 @@ function Star({ className, ...props }: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="19"
-      height="19"
       viewBox="0 0 19 19"
       fill="none"
       className={`block ${className}`}
