965907e2cf4c761c0dbb20e59fee3b0e318597bb
Mide <ibrahimayomide330@gmail.com>
2024-02-11 21:37:34 +0000

    feat: add custom error component

--- src/Components/Error.tsx
+++ src/Components/Error.tsx
@@ -3,14 +3,14 @@ import { useRouteError } from "react-router-dom";
 type Error = {
   message: string;
   statusText: string;
-  status: number;
+  status: number | string;
 };
 
 function Error() {
   const error = useRouteError() as Error;
   return (
     <>
-      <div className="grid place-content-center border-2 w-full h-screen">
+      <div className="grid place-content-center w-full my-20">
         <h1 className="text-custom_primary_500 font-medium">
           An error occured: {error.message}
         </h1>
@@ -23,3 +23,18 @@ function Error() {
 }
 
 export default Error;
+
+export function CustomError({ message, statusText, status }: Error) {
+  return (
+    <>
+      <div className="grid place-content-center w-full my-20">
+        <h1 className="text-custom_primary_500 font-medium">
+          An error occured: {message}
+        </h1>
+        <p className="text-custom_primary_400 pt-2">
+          {status} - {statusText}
+        </p>
+      </div>
+    </>
+  );
+}
