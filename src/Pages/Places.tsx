831028800639c21db7eb075d37bea86f09570baa
Mide <ibrahimayomide330@gmail.com>
2024-02-11 21:52:43 +0000

    feat: add contect provider

--- src/Pages/Places.tsx
+++ src/Pages/Places.tsx
@@ -1,4 +1,5 @@
 import { useRef, useEffect } from "react";
+import { FilterContextProvider } from "../FilterContext";
 import FilterTab from "@/Components/FilterTab";
 import Header from "@/Components/Header";
 import PlacesRender from "@/Components/PlacesRender";
@@ -11,10 +12,12 @@ function Places() {
   // reveal header shadow on scoll
   useEffect(() => {
     function toggleShadow() {
-      if (window.scrollY > 0) {
-        headerRef.current!.classList.add("shadow-visible");
-      } else {
-        headerRef.current!.classList.remove("shadow-visible");
+      if (headerRef.current != null) {
+        if (window.scrollY > 0) {
+          headerRef.current.classList.add("shadow-visible");
+        } else {
+          headerRef.current.classList.remove("shadow-visible");
+        }
       }
     }
 
@@ -27,20 +30,22 @@ function Places() {
 
   return (
     <>
-      {/* used fixed on sm screens to avoid header jittering on scroll*/}
-      {/* used sticky on lg screens to avoid header width glitch */}
-      <section
-        className="fixed lg:sticky top-0 z-20 w-full bg-white shadow-none transition-shadow"
-        ref={headerRef}
-      >
-        <Header />
-        <Divider axis="horizontal" />
-        <FilterTab />
-      </section>
-      <main className="container pt-[150px] sm:pt-[195px] lg:pt-0 mt-6 md:mt-4">
-        <PlacesRender />
-      </main>
-      <Footer />
+      <FilterContextProvider>
+        {/* used fixed on sm screens to avoid header jittering on scroll*/}
+        {/* used sticky on lg screens to avoid header width glitch */}
+        <section
+          className="fixed lg:sticky top-0 z-20 w-full bg-white shadow-none transition-shadow"
+          ref={headerRef}
+        >
+          <Header />
+          <Divider axis="horizontal" />
+          <FilterTab />
+        </section>
+        <main className="container pt-[150px] sm:pt-[195px] lg:pt-0 mt-6 md:mt-4">
+          <PlacesRender />
+        </main>
+        <Footer />
+      </FilterContextProvider>
     </>
   );
 }
