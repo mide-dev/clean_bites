5aa01efe306ae05a96d46675b2d3024d4b90d721
Mide <ibrahimayomide330@gmail.com>
2024-02-11 21:55:43 +0000

    feat: update api fetch

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,37 +1,76 @@
 import { Place } from "./types";
 
-// type ApiProp = {
-//   id?: number
-// }
+// set base url
+const baseURL = "http://127.0.0.1:8000/cleanbites_api/";
 
-export async function getPlaces(): Promise<Place[]> {
- 
-    const response = await fetch("/api/places");
-    if (!response.ok) {
-      throw {
-        message: "Failed to fetch Places",
-        statusText: response.statusText,
-        status: response.status
-      }
-    }
-    // simulate slow fetch
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-    const data = await response.json();
-    return data.places;
-  
+/**
+ * Fetch places from server
+ */
+export async function getPlaces(page = 0): Promise<Place[]> {
+  let response;
 
-  // 
-  // const response = await fetch("/api/placesDetail"); 
-  //   if (!response.ok) {
-  //     throw {
-  //       message: "Failed to fetch Places",
-  //       statusText: response.statusText,
-  //       status: response.status
-  //     }
-  //   }
-  //   // simulate slow fetch
-  //   // await new Promise((resolve) => setTimeout(resolve, 3000));
-  //   const data = await response.json();
-  //   return data;
+  if (page > 0) {
+    response = await fetch(`${baseURL}places/?page=${page}`);
+  } else {
+    response = await fetch(`${baseURL}places/`);
   }
-  
+  if (!response.ok) {
+    throw {
+      message: "Failed to fetch Places",
+      statusText: response.statusText,
+      status: response.status,
+    };
+  }
+  const data = await response.json();
+  return data.results;
+}
+
+// Fetch detailed info for a place
+export async function getPlaceDetail(place_id) {
+  const response = await fetch(`${baseURL}places/${place_id}`);
+  if (!response.ok) {
+    throw {
+      message: "Failed to fetch this Place",
+      statusText: response.statusText,
+      status: response.status,
+    };
+  }
+  const data = await response.json();
+  return data;
+}
+
+// Fetch reviews for a place
+export async function getPlaceReview(place_id) {
+  const response = await fetch(`${baseURL}places/${place_id}/reviews`);
+  if (!response.ok) {
+    throw {
+      message: "Failed to fetch reviews",
+      statusText: response.statusText,
+      status: response.status,
+    };
+  }
+  const data = await response.json();
+  return data;
+}
+
+// get a search result
+export async function getPlaceSearch(query: string, page: number = 0) {
+  let response;
+
+  if (page === 0) {
+    response = await fetch(`${baseURL}places/search/?query="${query}"`);
+  } else {
+    response = await fetch(
+      `${baseURL}places/search/?page=${page}&query="${query}"`
+    );
+  }
+  if (!response.ok) {
+    throw {
+      message: "No results available for your search",
+      statusText: response.statusText,
+      status: response.status,
+    };
+  }
+  const data = await response.json();
+  return data.results;
+}
