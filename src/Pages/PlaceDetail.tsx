6e1dae472a56aa1729e047343fdbf836d7c128d5
Mide <ibrahimayomide330@gmail.com>
2024-02-11 21:52:02 +0000

    refactor: move out sections into individual components

--- src/Pages/PlaceDetail.tsx
+++ src/Pages/PlaceDetail.tsx
@@ -1,31 +1,48 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Header from "@/Components/Header";
-import Map from "@/Components/Map/Map";
-import PlaceOffers from "@/Components/PlaceOffers";
-import PlaceRecommendation from "../Components/PlaceRecommendation";
+import PlaceDetailRecommendation from "@/Components/PlaceDetailRecommendation";
 import Footer from "../Components/Footer";
-import ImageSlider from "../Components/ImageSlider";
-import { UtensilsCrossed, PhoneCall, ChevronRight } from "lucide-react";
-import { useHygieneCheck, HygieneProp } from "../constants/HygieneCheck";
-import HygieneIcon from "@/assets/hygieneIcon";
-import Star from "@/assets/Star";
-import ReviewCard from "@/Components/ReviewCard";
-import { Button } from "@/Components/ui/button";
-import places from "../../mockData/places.json";
-import write from "../assets/write.png";
-
+import PlaceInfo from "../Components/PlaceInfo";
 import placesDetail from "../../mockData/PlacesDetail.json";
+import { CustomError } from "../Components/Error";
 import Divider from "@/Components/Divider";
+import PlaceOfferSection from "../Components/PlaceOfferSection";
+import PlaceReviewSection from "../Components/PlaceReviewSection";
+import { getPlaceDetail } from "../constants/api";
 
 function PlaceDetail() {
+  const params = useParams();
   const [placeDetail, setPlaceDetail] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    message: "",
+    statusText: "",
+    status: "",
+    isError: false,
+  });
 
-  // process & retrieve hygiene data
-  const hygieneResult = useHygieneCheck(placeDetail?.RatingValue);
+  // fetch place detail.
+  const fetchPlaceDetail = async () => {
+    try {
+      setLoading(true);
+      const place = await getPlaceDetail(params.place_id);
+      setPlaceDetail(place);
+    } catch (error) {
+      setError({
+        message: "Failed to fetch Places",
+        statusText: error.statusText,
+        status: error.status,
+        isError: true,
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
 
+  // call the function to fetch place
   useEffect(() => {
-    setPlaceDetail(placesDetail[0]);
+    fetchPlaceDetail();
   }, []);
 
   if (placeDetail) {
@@ -34,164 +51,33 @@ function PlaceDetail() {
         <Header className="hidden md:flex is-place-detail" />
         <Divider axis="horizontal" className="hidden md:block" />
         <main className="md:container is-place-detail text-sm mb-10">
-          {/* PLACE INFO */}
-          <section className="flex flex-col">
-            {/* Image */}
-            <div className="placeCard md:order-3">
-              <ImageSlider
-                images={placeDetail.photos}
-                scrollSpeed={1000}
-                className="block max-h-[400px] lg:max-h-[500px] w-full md:rounded-xl"
-                displayScrollBtn={true}
-              />
-            </div>
-            {/*  title */}
-            <div className="flex items-center place_detail_section md:order-1">
-              <h1 className="my-2">
-                {placeDetail.BusinessName}
-                <span className="px-2">&#8226;</span>
-              </h1>
-              <div className="text-base">$$$</div>
-            </div>
-            {/* hygiene */}
-            <div className="place_detail_section md:order-4">
-              {/* conditionally render hygiene */}
-              {hygieneResult === "Awaiting Inspection" ? (
-                <div className={`flex pt-2 items-center gap-x-2 py-2`}>
-                  Health & Hygiene Result -
-                  <p className="block font-medium">Awaiting Inspection</p>
-                </div>
-              ) : (
-                <>
-                  <div className="flex gap-x-1 pb-3">
-                    <HygieneIcon className={`${hygieneResult.fill}`} />
-                    <p>
-                      Health & Hygiene Result -{" "}
-                      <span className="font-medium">
-                        {hygieneResult.ratingText}
-                      </span>
-                    </p>
-                  </div>
-                  <div className="progress-bar w-full h-5 bg-[#ccc] rounded-full">
-                    <div
-                      className={`grid place-content-center progress-fill h-full w-${hygieneResult.hygieneScore}% rounded-full ${hygieneResult.bg}`}
-                    >
-                      <p className="text-white font-medium">
-                        {hygieneResult.hygieneScore}%
-                      </p>
-                    </div>
-                  </div>
-                  {/* mobile-divider */}
-                  <Divider
-                    axis="horizontal"
-                    className="bg-slate-300 md:hidden"
-                  />
-                </>
-              )}
-            </div>
-
-            {/* google review group */}
-            <div className=" gap-x-[0.1rem] place_detail_section md:order-2">
-              <div className="flex">
-                <Star className="w-4 fill-black inline-block" />
-                <p className="inline-block">
-                  4.92 <span className="px-[0.2rem] inline-block">&#8226;</span>
-                  <button className="underline inline-block">
-                    723 Google Reviews
-                  </button>
-                </p>
-              </div>
-              <p className="pt-1">
-                60 Spring Gardens,<span className="px-[0.2rem]"></span> London
-              </p>
-            </div>
-            {/* open time */}
-            <div className="flex gap-x-2 items-center place_detail_section md:order-5">
-              <UtensilsCrossed className="w-5 stroke-custom_primary_500" />
-              <p>Open 11:00 AM - 11:00 PM</p>
-              <span>&#8226;</span>
-              <button className="underline">See hours</button>
-            </div>
-            {/* contact call */}
-            <div className="place_detail_section md:order-6">
-              <div className="flex gap-x-2 items-center">
-                <PhoneCall className="w-5 stroke-custom_primary_500" />
-                <p>0191 260 5880</p>
-              </div>
-              {/* mobile-divider */}
-              <Divider axis="horizontal" className="bg-slate-300 md:hidden" />
-            </div>
-          </section>
-
-          {/* PLACE OFFERS */}
-          <section className="place_detail_section">
-            <h2>What this place offers</h2>
-            <PlaceOffers data={placeDetail.offers[0]} />
-            <Divider axis="horizontal" className="bg-slate-300" />
-          </section>
-          {/* Place Map */}
-          <section className="place_detail_section">
-            <div className="mb-2">
-              <h2>Where you're going</h2>
-            </div>
-            <Map
-              latitude={51.5072}
-              longitude={0.1276}
-              className="mx-auto w-full h-[220px] sm:h-[300px] md:h-[450px]"
-            />
-            <Divider axis="horizontal" className="bg-slate-300" />
-          </section>
-          {/* place review */}
-          <section className="place_detail_section">
-            <h2>Recommended Reviews</h2>
-            <div className="flex gap-x-1">
-              <Star className="w-4 fill-custom_accent inline-block" />
-              <h3 className="">4.93 from 207 reviews</h3>
-            </div>
-            <Button
-              variant="outline"
-              className="flex items-center gap-x-[0.125rem] cursor-pointer hover:shadow"
-            >
-              <img src={write} className="pb-1" />
-              <span>Write a review</span>
-            </Button>
+          <PlaceInfo data={placeDetail} />
+          <section className="place-detail-body relative">
             <div>
-              {placeDetail.reviews.map((review) => (
-                <>
-                  <ReviewCard data={review} key={review.time} />
-                  <Divider
-                    axis="horizontal"
-                    className="bg-slate-300 last:hidden"
-                  />
-                </>
-              ))}
+              <PlaceOfferSection data={placeDetail} />
+              <PlaceReviewSection placeData={placeDetail} />
             </div>
-            <Button
-              variant="outline"
-              className="flex items-center gap-x-[0.125rem] cursor-pointer hover:shadow"
-            >
-              <span>More reviews on Google</span>
-              <ChevronRight className="stroke-slate-700" />
-            </Button>
-            <Divider axis="horizontal" className="bg-slate-300" />
-            {/* Explore */}
-            <div className="bg-[#FFFBFC] md:bg-white rounded-xl ">
-              <h2>
-                Explore places similar to{" "}
-                <span className="font-medium">{placeDetail.BusinessName}</span>
-              </h2>
-              <div className="rounded-xl overflow-scroll no-scrollbar scroll-smooth">
-                <PlaceRecommendation
-                  place_id={placeDetail.place_id}
-                  key={placeDetail.place_id}
-                  className="flex gap-x-4 overflow-scroll no-scrollbar scroll-smooth"
-                />
-              </div>
-            </div>
+            <PlaceDetailRecommendation data={placeDetail} />
           </section>
         </main>
         <Footer className="is-place-detail" />
       </>
+    );
+  }
+
+  // display initial loading screen
+  if (loading) {
+    return <div>I'm Loading details...</div>;
+  }
+
+  // if error, display error to user
+  if (error.isError) {
+    return (
+      <CustomError
+        message={error.message}
+        status={error.status}
+        statusText={error.statusText}
+      />
     );
   }
 }
