b48e1617dd219d770b5518d91c5363e7728a9bf9
Mide <ibrahimayomide330@gmail.com>
2024-02-11 21:50:09 +0000

    refactor:

--- src/Components/ReviewCard.tsx
+++ src/Components/ReviewCard.tsx
@@ -4,29 +4,29 @@ import profileImage from "../assets/profileImage.png";
 import Rating from "react-rating";
 import Star from "@/assets/Star";
 
-// type ReviewCardProp = {object[]};
-
-function ReviewCard({ data }) {
+function ReviewCard({ author_photo_url, author_name, datetime, text, rating }) {
   return (
     <>
-      <div className="flex gap-x-2 items-center">
+      <div className="flex gap-x-2 items-center my-4">
         <Avatar className="w-12 h-12">
-          <AvatarImage src={data.profile_photo_url} />
-          <AvatarFallback>CN</AvatarFallback>
+          <AvatarImage src={author_photo_url} />
+          <AvatarFallback className="bg-gray-400 text-lg font-medium text-white">
+            {author_name.substring(0, 2).toUpperCase()}
+          </AvatarFallback>
         </Avatar>
         <div>
-          <h4 className="font-medium pb-1">{data.author_name}</h4>
-          <p>{data.relative_time_description}</p>
+          <h4 className="font-medium pb-1">{author_name}</h4>
+          <p>{datetime}</p>
         </div>
       </div>
       <Rating
         emptySymbol={<Star className="w-5 stroke-black inline-block" />}
         fullSymbol={<Star className="w-5 fill-black inline-block" />}
-        initialRating={data.rating}
+        initialRating={rating}
         fractions={10}
         readonly
       />
-      <p className="min-w-[250px] xs:min-w-[300px]">{data.text}</p>
+      <p className="min-w-[250px] xs:min-w-[300px] py-4">{text}</p>
     </>
   );
 }
