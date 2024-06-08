252683bdea1128fb50bcb0adb4c86a3c565e0b1d
Mide <ibrahimayomide330@gmail.com>
2024-06-08 13:47:56 +0100

    feat: replaced search component

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Layout from "./Components/Layout";
 import Places from "./Pages/Places";
 import TopPicks from "./Pages/TopPicks";
 import WhyUs from "./Pages/WhyUs";
+import Search from "./Pages/Search";
 import PlacesLoader from "./Components/PlacesLoader";
 import Error from "./Components/Error";
 import PlaceDetail from "./Pages/PlaceDetail";
@@ -24,6 +25,7 @@ const router = createBrowserRouter(
         <Route path="/" element={<Layout />}>
           <Route path="top-picks" element={<TopPicks />} />
           <Route path="why-us" element={<WhyUs />} />
+          <Route path="search" element={<Search />} />
         </Route>
       </Route>
     </>
