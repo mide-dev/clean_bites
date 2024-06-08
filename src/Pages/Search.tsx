1bbd7a6cc6e89c83faa1a5f52322d7546b4ffa23
Mide <ibrahimayomide330@gmail.com>
2024-06-08 13:54:11 +0100

    no update

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -1,5 +1,37 @@
+import { useState, useEffect, useRef } from "react";
+import { getPlaceSearch } from "../constants/api";
+import { CustomError } from "../Components/Error";
+import { Link } from "react-router-dom";
+import PlacesCard from "../Components/PlacesCard";
+
 function Search() {
-  return <>This is search</>;
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({
+    message: "",
+    statusText: "",
+    status: "",
+    isError: false,
+  });
+
+  // const fetchPlaceSearch = async () {
+  //   const placesFetch = await getPlaceSearch(selectedItem, currentPage);
+  // }
+
+  return (
+    <main className="md:container shrinked-container text-sm mb-10">
+      <div className="places-grid">
+        {items.map((place, index) => (
+          <Link to={`/places/${place.id}`} key={`${selectedItem}_${place.id}`}>
+            <PlacesCard
+              ref={index === items.length - 1 ? setLastElement : null}
+              {...place}
+            />
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default Search;
