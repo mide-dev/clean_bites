c13635c2b7673e9af591345db23c1981c1621cdf
Mide Ibrahim <mide@Mides-MacBook-Pro.local>
2024-07-06 16:08:49 +0100

    feat: replace dummy recommendations with real requests

--- src/Components/PlaceRecommendation.tsx
+++ src/Components/PlaceRecommendation.tsx
@@ -1,23 +1,47 @@
-import { Card } from "./ui/card";
-import places from "../../mockData/places.json";
 import PlacesCard from "./PlacesCard";
+import { useEffect, useState } from "react";
+import { getRecommendedPlaces } from "@/constants/api";
+import { useNavigate } from "react-router-dom";
+import LoadingState from "./LoadingState";
+import { Place } from "@/constants/types";
 
-function PlaceRecommendation(place_id) {
-  const currentPlace = places.filter(
-    (place) => place.place_id === place_id.place_id
-    );
-    
-  // i ll use the place_id to get recommendation from backend
-  
-  
-  return (
-    <div className="no-scrollbar flex lg:flex-col overflow-x-auto gap-x-4"> 
-      {
-        places.map(place => (
-          <PlacesCard key={place.place_id} {...place} backround={true} className=" w-[270px] md:w-[300px] shrink-0 my-4"/>
-        ))
+function PlaceRecommendation() {
+  const [placeData, setPlaceData] = useState<Place[]>([]);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setLoading(true);
+    (async () => {
+      try {
+        const recommendedPlaces = await getRecommendedPlaces();
+        setPlaceData([...recommendedPlaces]);
+      } catch (error) {
+        return;
+      } finally {
+        setLoading(false);
       }
-      
+    })();
+  }, []);
+
+  const handleNavigation = (id: number) => {
+    navigate(`/places/${id}`);
+    window.location.reload();
+  };
+
+  return (
+    <div className="no-scrollbar flex lg:flex-col overflow-x-auto gap-x-4 scroll-smooth">
+      {loading && <LoadingState />}
+      {placeData.map((place) => (
+        <div onClick={() => handleNavigation(place.id)}>
+          <PlacesCard
+            key={place.id}
+            {...place}
+            backround={true}
+            className=" w-[270px] md:w-[300px] shrink-0 my-4 cursor-pointer"
+          />
+        </div>
+      ))}
     </div>
   );
 }
