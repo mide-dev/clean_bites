87f08910dc0ee6480c0eff302745febfb93451df
Mide Ibrahim <mide@Mides-MacBook-Pro.local>
2024-07-06 16:05:43 +0100

    Refactor:

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,49 @@ import {
   createRoutesFromElements,
   Route,
 } from "react-router-dom";
+import { AuthProvider } from "./AuthProvider";
 
 import "./App.css";
 
 import Layout from "./Components/Layout";
 import Places from "./Pages/Places";
-import TopPicks from "./Pages/TopPicks";
-import WhyUs from "./Pages/WhyUs";
 import Search from "./Pages/Search";
 import PlacesLoader from "./Components/PlacesLoader";
 import Error from "./Components/Error";
 import PlaceDetail from "./Pages/PlaceDetail";
+import Profile from "./Pages/Profile";
+import PlacesReviewed from "./Pages/PlacesReviewed";
+import Favorites from "./Pages/Favorites";
+import LoginSignup from "./Pages/auth/LoginSignup";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route path="/" errorElement={<Error />}>
-        <Route path="/" element={<Places />} loader={PlacesLoader} />
-        <Route path="/places/:place_id" element={<PlaceDetail />} />
-        <Route path="/" element={<Layout />}>
-          <Route path="top-picks" element={<TopPicks />} />
-          <Route path="why-us" element={<WhyUs />} />
-          <Route path="search" element={<Search />} />
-        </Route>
+      <Route
+        index
+        element={<Places />}
+        loader={PlacesLoader}
+        errorElement={<Error />}
+      />
+      <Route path="/" element={<Layout />} errorElement={<Error />}>
+        <Route path="places/:place_id" element={<PlaceDetail />} />
+        <Route path="search" element={<Search />} />
+        <Route path="login" element={<LoginSignup />} />
+        <Route path="signup" element={<LoginSignup />} />
+        <Route path="favorites" element={<Favorites />} />
+        <Route path="placesreviewed" element={<PlacesReviewed />} />
+        <Route path="profile" element={<Profile />} />
       </Route>
     </>
   )
 );
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
 }
 
 export default App;
